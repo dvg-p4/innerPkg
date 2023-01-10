10354bd651817be5afbc73b9daaa7fde6e73bf6b
dvg-p4 <dgealow@paradigm4.com>
2023-01-10 10:45:17 -0500

    Bump version to 0.1.0 -- beta release!

--- R/inner_function.R
+++ R/inner_function.R
@@ -10,5 +10,5 @@ inner_function <- function(x) {
   print("This is inner_function()")
   print("You gave me")
   print(x)
-  print("This is the alpha version of innerPkg!")
+  print("This is the beta version 0.1.0 of innerPkg!")
 }
