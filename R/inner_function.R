990193e1b2bf100e6e792c3601efe01a119dd589
dvg-p4 <dgealow@paradigm4.com>
2023-01-10 10:41:18 -0500

    Add boilerplate

--- R/inner_function.R
+++ R/inner_function.R
@@ -1,3 +1,11 @@
+#' Print the argument and some version info
+#'
+#' @param x The argument to print
+#'
+#' @export
+#'
+#' @examples
+#' inner_function(7)
 inner_function <- function(x) {
   print("This is inner_function()")
   print("You gave me")
